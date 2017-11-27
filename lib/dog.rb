60000daf0acd7997585fefead62997bc75c0030b
KellyStamps <kellydsample@gmail.com>
2017-11-26 19:51:55 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
   # code goes here
+  attr_accessor :mood
+  attr_reader :name
+  @@all = []
+
+  def initialize (name)
+    @name = name
+    @@all << self
+    @mood = "nervous"
+  end
+
+  def all
+    @@all
+  end
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,74 @@
 class Owner
   # code goes here
+  attr_accessor :name, :pets, :mood
+
+  @@all = []
+  @final = {}
+
+  def initialize(species)
+    @@species = species
+    @@all << self
+    @pets = {:fishes => [], :cats => [], :dogs => []}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def mood
+    @mood
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def species
+    @@species
+  end
+
+  def say_species
+    "I am a #{@@species}."
+  end
+
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    self.pets[:fishes] << new_fish
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    self.pets[:dogs] << new_dog
+  end
+
+  def buy_cat(name)
+    new_cat = Cat.new(name)
+    self.pets[:cats] << new_cat
+  end
+
+  def walk_dogs
+    self.pets[:dogs].each {|animal| animal.mood = "happy"  }
+  end
+
+  def play_with_cats
+    self.pets[:cats].each {|animal| animal.mood = "happy"  }
+  end
+
+  def feed_fish
+    self.pets[:fishes].each {|animal| animal.mood = "happy"  }
+  end
+
+  def sell_pets
+    self.pets.each {|species, info| info.each {|animal| animal.mood = "nervous"  }  }
+    pets.clear
+  end
+
+  def list_pets
+    "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
+  end
+
 end
